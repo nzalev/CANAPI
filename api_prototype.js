b58a5ac3c502fb71ba37a70e68aad24c67d0918b
Noah Zalev <github@nzalev.ca>
2022-06-21 10:58:02 -0400

    Added bulk frame processing

--- api_prototype.js
+++ api_prototype.js
@@ -66,4 +66,53 @@ app.post('/frames', (req, res) => {
     return res.send('ack\n');
 });
 
+app.post('/frames/bulk', (req, res) => {
+
+    if ( ! checkAPIKey(req) ) {
+        res.status(403);
+        return res.send('Forbidden\n');
+    }
+
+    if (req.body.length == undefined) {
+        res.status(400);
+        return res.send('Invalid Format\n');
+    }
+
+    var frames = req.body;
+    var time_received = new Date();
+    var frame_list = [];
+
+    frames.forEach(f => {
+        if ( ! checkNulls(f,
+                ['vehicle_id', 'arbitration_id', 'data_len', 'data_string']) ) {
+            res.status(400);
+            return res.send('Missing parameters\n');
+        }
+
+        var { vehicle_id, arbitration_id, data_len, data_string } = f;
+        var frame = {
+            vehicle_id,
+            arbitration_id,
+            data_len,
+            data_string,
+            time_received: time_received
+        };
+        frame_list.push(frame);
+    });
+
+    try {
+        var vid = frame_list[0]['vehicle_id'];
+        const collection = db.collection("vehicle_" + String(vid));
+        collection.insertMany(frame_list);
+    } catch (e) {
+        console.log(e);
+        res.status(500);
+        res.send('err\n');
+    }
+
+    res.status(200);
+    res.send('ack\n');
+});
+
+
 app.listen(3000, 'localhost');
